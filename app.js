4a55fee33de3949e3d54e0bd1a7087acc098089f
WonYongHyeon <quin1392@gmail.com>
2023-10-11 22:00:59 +0900

    TIL 관련 express get 메소드 완료

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const til = [
+  {
+    title: "TIL.1 | 프론트엔드 클라이언트 AWS 배포",
+    link: "https://velog.io/@quin1392/프론트엔드-클라이언트-AWS-배포",
+  },
+  {
+    title: "TIL.2 | 탐욕 알고리즘(Greedy Algorithm)",
+    link: "https://velog.io/@quin1392/TIL.2-알고리즘-탐욕-알고리즘",
+  },
+  {
+    title: "TIL.3 | Union-Find 알고리즘",
+    link: "https://velog.io/@quin1392/TIL.3-Union-Find-알고리즘",
+  },
+];
+
+// 모든 도메인에 대해 cors 개방
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(til);
+});
+
+app.listen(port, () => {
+  console.log(`서버 실행, http://localhost:${port}`);
+});
+
+// // 라우터 체인 : 하나의 라우트 경로로 각 라우트 메소드 처리
+// app
+//   .route("/til")
+//   .get((req, res) => {
+//     // HTTP 메소드 GET 요청에 대한 조회 처리
+//     res.send("고객 정보 조회");
+//   })
+//   .post((req, res) => {
+//     // HTTP 메소드 POST 요청에 대한 저장 처리
+//     res.send("신규 고객 추가");
+//   });
