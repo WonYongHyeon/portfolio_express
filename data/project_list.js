f73d50267175dc880993355bc578c60691c35eab
WonYongHyeon <quin1392@gmail.com>
2023-10-14 19:26:56 +0900

    TIL data, project data 서버 설정

---
+++ data/project_list.js
@@ -0,0 +1,28 @@
+export const projectList = [
+  {
+    category: "Team Project",
+    title: "Farm You",
+    desc: "로컬 푸드 및 못난이 농산물 온라인 마켓 서비스",
+    skill: "[프론트엔드, 배포] Typescript, React, Next.js, GraphQL, Emotion",
+    done: `● 반응형 디자인 전담
+● Type 수정 전담
+● 마이(구매자, 판매자) 페이지 개발 전담
+● 상품 상세 페이지 개발 전담 / 댓글 및 대댓글
+● 상품 수정 페이지 개발 / 웹 에디터 이미지 적용
+● 결제 페이지 개발 전담 / 상품별 환불 구현
+● AWS 배포 전담`,
+    youtubeId: "anttjHWTN5c",
+  },
+  {
+    category: "Personal Project",
+    title: "기프티마켓",
+    desc: "기프티콘 거래 마켓 서비스",
+    skill:
+      "[기획, 프론트엔드, 디자인] Typescript, React, Next.js, GraphQL, Emotion",
+    done: `● 구입한 기프티콘 카카오톡 공유 서비스 구현
+● 사이트 설명 랜딩 페이지 구현
+● 리뷰 게시판 구현
+● 기프티콘 거래 게시판 구현
+● AWS 동적 배포`,
+  },
+];
---
+++ data/til_list.js
@@ -0,0 +1,59 @@
+export const tilList = [
+  {
+    title: "프론트엔드 클라이언트 AWS 배포",
+    link: "https://velog.io/@quin1392/프론트엔드-클라이언트-AWS-배포",
+  },
+  {
+    title: "탐욕 알고리즘(Greedy Algorithm)",
+    link: "https://velog.io/@quin1392/TIL.2-알고리즘-탐욕-알고리즘",
+  },
+  {
+    title: "Union-Find 알고리즘",
+    link: "https://velog.io/@quin1392/TIL.3-Union-Find-알고리즘",
+  },
+  {
+    title: "크루스칼 알고리즘 (Kruskal Algorithm)",
+    link: "https://velog.io/@quin1392/TIL.4-크루스칼-알고리즘-Kruskal-Algorithm",
+  },
+  {
+    title: "웹팩(Webpack)과 바벨(Babel)",
+    link: "https://velog.io/@quin1392/TIL.5-웹팩Webpack과-바벨Babel",
+  },
+  {
+    title: "리액트(React)와 넥스트(Next.js)의 차이",
+    link: "https://velog.io/@quin1392/TIL.6-리액트React와-넥스트Next.js의-차이",
+  },
+  {
+    title: "리액트(React) 라우팅과 넥스트(Next.js) 라우팅",
+    link: "https://velog.io/@quin1392/TIL.7-리액트React-라우팅과-넥스트Next.js-라우팅수정중",
+  },
+  {
+    title: "input 창 한 번에 관리하기",
+    link: "https://velog.io/@quin1392/TIL.8-여러-input-창-관리하기",
+  },
+  { title: "DFS, BFS", link: "https://velog.io/@quin1392/TIL-9.-DFS-BFS" },
+  {
+    title: "CSS 애니메이션",
+    link: "https://velog.io/@quin1392/TIL.10-CSS-애니메이션",
+  },
+  {
+    title: "Git에 대하여",
+    link: "https://velog.io/@quin1392/TIL.11-Git에-대하여",
+  },
+  {
+    title: "Styled-Component",
+    link: "https://velog.io/@quin1392/TIL.12-Styled-Component",
+  },
+  {
+    title: "리덕스(redux)와 리덕스 툴킷(redux-toolkit)",
+    link: "https://velog.io/@quin1392/TIL.13-리덕스redux와-리덕스-툴킷redux-toolkit",
+  },
+  {
+    title: "주석 효과적으로 달기",
+    link: "https://velog.io/@quin1392/TIL.14-주석-효과적으로-달기",
+  },
+  {
+    title: "Git - branch, commit 메시지, issue",
+    link: "https://velog.io/@quin1392/TIL.15-Git-branch-commit-메시지-issue",
+  },
+];
