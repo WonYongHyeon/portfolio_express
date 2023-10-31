536841888963d0fc3945d49148f00e1d9feebe5b
WonYongHyeon <quin1392@gmail.com>
2023-10-31 22:21:40 +0900

    cors 오류

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3002;
 
 // 모든 도메인에 대해 cors 개방
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.get("/TIL", (req, res) => {
   const search = req.query.search ? req.query.search.toLowerCase() : "";
