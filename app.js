f74f8ef374a708d7891216081a52a66462559082
WonYongHyeon <quin1392@gmail.com>
2023-10-31 19:05:42 +0900

    Cannot Get 오류 수정

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { tilList } from "./data/til_list.js";
 import { projectList } from "./data/project_list.js";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
 const port = 3002;
@@ -9,9 +10,18 @@ const port = 3002;
 // 모든 도메인에 대해 cors 개방
 app.use(cors());
 
+// 새로고침 시 Cannot Get 오류 방지
+app.use(
+  createProxyMiddleware(["/project", "/skill", "til"], {
+    target: "http://localhost:3000",
+    changeOrigin: true,
+  })
+);
+
 app.get("/TIL", (req, res) => {
   console.log(req.query.search);
   const search = req.query.search ? req.query.search.toLowerCase() : "";
+  const page = req.query.page ? req.query.page : 1;
   if (search === "") {
     res.send(tilList);
   } else if (search !== "") {
