53c2e7c26d280ae7a0bca040bcc562eb463740e5
WonYongHyeon <quin1392@gmail.com>
2023-11-03 22:46:18 +0900

    TIL 리스트 Page 기능 추가

--- app.js
+++ app.js
@@ -8,20 +8,27 @@ const port = 3002;
 
 // 모든 도메인에 대해 cors 개방
 app.use(cors());
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-// app.use(cors({ credentials: true, origin: "https://yonghyeon.site" }));
 
 app.get("/TIL", (req, res) => {
   const search = req.query.search ? req.query.search.toLowerCase() : "";
   const page = req.query.page ? req.query.page : 1;
+
+  // 검색어로 배열 필터링
+  let filterList = [];
   if (search === "") {
-    res.send(tilList);
+    filterList = tilList;
   } else if (search !== "") {
-    const filterList = tilList.filter(
+    filterList = tilList.filter(
       (el) => el.title.toLowerCase().indexOf(search) != -1
     );
-    res.send(filterList);
   }
+
+  // 필터링 된 배열 길이로 page 수 전송
+  const pageLength = Math.ceil(filterList / 10);
+  // 필터링 된 배열로 page 구분 후 전송
+  const sliceList = filterList.slice((page - 1) * 10, page * 10);
+
+  res.json({ pageLength: pageLength, tilList: sliceList });
 });
 
 app.get("/project", (req, res) => {
