6f666c45491334ad991e599c85f7a00eed9e8cc3
WonYongHyeon <quin1392@gmail.com>
2023-11-07 22:14:34 +0900

    TIL post 등록 기능

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = 3002;
 
 // 모든 도메인에 대해 cors 개방
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/TIL", (req, res) => {
   const search = req.query.search ? req.query.search.toLowerCase() : "";
@@ -22,9 +24,8 @@ app.get("/TIL", (req, res) => {
       (el) => el.title.toLowerCase().indexOf(search) != -1
     );
   }
-
   // 필터링 된 배열 길이로 page 수 전송
-  const pageLength = Math.ceil(filterList / 10);
+  const pageLength = Math.ceil(filterList.length / 10);
   // 필터링 된 배열로 page 구분 후 전송
   const sliceList = filterList.slice((page - 1) * 10, page * 10);
 
@@ -35,8 +36,31 @@ app.get("/project", (req, res) => {
   res.send(projectList);
 });
 
+app.post("/TIL/registration", (req, res) => {
+  if (!req.body.title || !req.body.url) {
+    res.json({
+      success: false,
+    });
+    return;
+  }
+
+  const tilListLength = tilList.length;
+
+  const til = {
+    order: `TIL.` + String(tilListLength + 1),
+    title: req.body.title,
+    link: req.body.url,
+  };
+
+  tilList.unshift(til);
+
+  console.log(til, tilList);
+
+  res.json({
+    success: true,
+  });
+});
+
 app.listen(port, () => {
   console.log(`서버 실행, http://localhost:${port}`);
 });
-
-app.post("/TIL");
