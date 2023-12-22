5eca234598e92e2ae471dad2523ae19dccbd1679
WonYongHyeon <quin1392@gmail.com>
2023-12-22 23:23:05 +0900

    Introduce 데이터 구조 변경

--- routes/firestore/Introduce/introduceRead.js
+++ routes/firestore/Introduce/introduceRead.js
@@ -19,7 +19,10 @@ export const introduceRead = async (db) => {
   const aboutData = [];
   const etcData = [];
 
-  //   for await (const doc of snapshot1) {
+  const introduceArr = [];
+  const introduceIdArr = [];
+
+  //   for (const doc in snapshot1) {
   //     const introduceDataDescDoc = await db
   //       .collection("data")
   //       .doc(doc.id)
@@ -39,27 +42,34 @@ export const introduceRead = async (db) => {
 
   //     introduceData.push(obj);
   //   }
-  console.log(snapshot1);
+
   await snapshot1.forEach(async (doc) => {
-    // desc 데이터
-    const introduceDataDescDoc = await db
-      .collection("data")
-      .doc(doc.id)
-      .collection("desc")
-      .doc("desc")
-      .get();
-
-    // desc 데이터 배열로 변경
-    const descObj = await introduceDataDescDoc.data();
-    const descArr = Object.keys(descObj).map((item) => descObj[item]);
-
     const obj = {
       id: doc.id,
       ...doc.data(),
-      desc: descArr,
+      //   desc: descArr,
     };
 
-    introduceData.push(obj);
+    introduceIdArr.push(doc.id);
+    introduceArr.push(obj);
+  });
+  // desc 데이터
+  const introduceDataDescDoc = await db
+    .collection("data")
+    .doc(doc.id)
+    .collection("desc")
+    .doc("desc")
+    .get();
+
+  // desc 데이터 배열로 변경
+  const descObj = await introduceDataDescDoc.data();
+  const descArr = Object.keys(descObj).map((item) => descObj[item]);
+
+  introduceArr.map((doc, idx) => {
+    introduceData.push({
+      ...doc,
+      desc: descArr[idx],
+    });
   });
 
   snapshot2.forEach((doc) => {
