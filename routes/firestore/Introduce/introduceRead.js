559e60812dc9dd93c97b1e76e3d114dae41e8e29
WonYongHyeon <quin1392@gmail.com>
2023-12-27 22:35:25 +0900

    Introduce 페이지 data backend로 이동

--- routes/firestore/Introduce/introduceRead.js
+++ routes/firestore/Introduce/introduceRead.js
@@ -22,71 +22,26 @@ export const introduceRead = async (db) => {
   const introduceArr = [];
   const introduceIdArr = [];
 
-  //   for (const doc in snapshot1) {
-  //     const introduceDataDescDoc = await db
-  //       .collection("data")
-  //       .doc(doc.id)
-  //       .collection("desc")
-  //       .doc("desc")
-  //       .get();
-
-  //     // desc 데이터 배열로 변경
-  //     const descObj = await introduceDataDescDoc.data();
-  //     const descArr = Object.keys(descObj).map((item) => descObj[item]);
-
-  //     const obj = {
-  //       id: doc.id,
-  //       ...doc.data(),
-  //       desc: descArr,
-  //     };
-
-  //     introduceData.push(obj);
-  //   }
-
   await snapshot1.forEach(async (doc) => {
     const obj = {
       id: doc.id,
       ...doc.data(),
-      //   desc: descArr,
     };
 
     introduceIdArr.push(doc.id);
     introduceArr.push(obj);
   });
 
-  //   for await (const id of introduceIdArr) {
-  //     const introduceDataDescDoc = await db
-  //       .collection("data")
-  //       .doc(id)
-  //       .collection("desc")
-  //       .doc("desc")
-  //       .get();
-
-  //     introduceData.push({
-  //       ...introduceArr[idx],
-  //       desc: introduceDataDescDoc.data(),
-  //     });
-  //   }
-
   for (let idx = 0; idx < introduceIdArr.length; idx++) {
     const id = introduceIdArr[idx];
-
     const introduceDataDescDoc = await db
       .collection("data")
       .doc(id)
       .collection("desc")
       .doc("desc")
       .get();
-
-    let descArr = [];
-
-    // introduceDataDescDoc.forEach((doc) => {
-    //   descArr.push(doc.data());
-    // });
-
-    // console.log(introduceDataDescDoc.data());
-
     const introduceDataDescObj = introduceDataDescDoc.data();
+    const descArr = [];
 
     for (let objKey in introduceDataDescObj) {
       if (introduceDataDescObj.hasOwnProperty(objKey)) {
@@ -96,27 +51,9 @@ export const introduceRead = async (db) => {
 
     introduceData.push({
       ...introduceArr[idx],
-      //   desc: introduceDataDescDoc.data(),
       desc: descArr,
     });
   }
-
-  // desc 데이터
-  //   await introduceIdArr.map(async (id, idx) => {
-  //     const introduceDataDescDoc = await db
-  //       .collection("data")
-  //       .doc(id)
-  //       .collection("desc")
-  //       .doc("desc")
-  //       .get();
-
-  //     console.log(introduceDataDescDoc.data());
-
-  //     introduceData.push({
-  //       ...introduceArr[idx],
-  //       desc: introduceDataDescDoc.data(),
-  //     });
-  //   });
 
   snapshot2.forEach((doc) => {
     aboutData.push({ id: doc.id, ...doc.data() });
@@ -125,8 +62,6 @@ export const introduceRead = async (db) => {
   snapshot3.forEach((doc) => {
     etcData.push({ id: doc.id, ...doc.data() });
   });
-
-  //   console.log(introduceArr, introduceData);
 
   return [introduceData, aboutData, etcData];
 };
